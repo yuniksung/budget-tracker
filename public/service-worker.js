077b8c8c0ae8c851d9e673951e2975afd4c997dc
Joshua Sung <yuniksung@gmail.com>
2021-03-05 23:04:00 -0600

    in Index.html, added a fucntion to be able to engage  with 'service-worker.js'

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,86 +1,54 @@
-const STATIC_CACHE = "static-cache-v1";
-const DATA_CACHE = "data-cache-v1";
 const FILES_TO_CACHE = [
-  "/",
-  "/dist/index.bundle.js",
-  "/assets/css/styles.css",
-  "/dist/auto/icon_96x96.png",
-  "/dist/auto/icon_128x128.png",
-  "/dist/auto/icon_192x192.png",
-  "/dist/auto/icon_256x256.png",
-  "/dist/auto/icon_384x384.png",
-  "/dist/auto/icon_512x512.png",
-  "/dist/manifest.json",
+	"./index.html",
+	"./css/styles.css",
+	"./js/index.js",
+	"./js/idb.js",
 ];
 
-// install service worker:
-// 1. cache static files
-// 2. cache data data
-self.addEventListener("install", function (event) {
-  event.waitUntil(
-    Promise.all([
-      caches.open(STATIC_CACHE).then(function (cache) {
-        return cache.addAll(FILES_TO_CACHE);
-      }),
-      caches.open(DATA_CACHE).then(function (cache) {
-        return cache.add("/api/transaction");
-      }),
-    ])
-  );
+const APP_PREFIX = "Budget_Tracker-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 
-  self.skipWaiting();
+self.addEventListener("install", function (e) {
+	e.waitUntil(
+		caches.open(CACHE_NAME).then(function (cache) {
+			console.log("installing cache : " + CACHE_NAME);
+			return cache.addAll(FILES_TO_CACHE);
+		})
+	);
 });
 
-// clear out old caches in the activate stage
-self.addEventListener("activate", function (event) {
-  const allowedCaches = [STATIC_CACHE, DATA_CACHE];
+self.addEventListener("activate", function (e) {
+	e.waitUntil(
+		caches.keys().then(function (keyList) {
+			let cacheKeeplist = keyList.filter(function (key) {
+				return key.indexOf(APP_PREFIX);
+			});
+			cacheKeeplist.push(CACHE_NAME);
 
-  event.waitUntil(
-    caches.keys().then(function (cacheNames) {
-      return Promise.all(
-        cacheNames.map(function (cacheName) {
-          if (!allowedCaches.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+			return Promise.all(
+				keyList.map(function (key, i) {
+					if (cacheKeeplist.indexOf(key) === -1) {
+						console.log("deleting cache : " + keyList[i]);
+						return caches.delete(keyList[i]);
+					}
+				})
+			);
+		})
+	);
 });
 
-// fetch event listener:
-// GET /api/transaction - return cached data on fail
-// any other fetch - return any matching static cache or necessary response
-self.addEventListener("fetch", function (event) {
-  if (
-    event.request.method === "GET" &&
-    event.request.url.includes("/api/transaction")
-  ) {
-    event.respondWith(
-      caches.open(DATA_CACHE).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            if (response.status === 200) {
-              cache.put(event.request, response.clone());
-              return response;
-            }
-          })
-          .catch(err => {
-            return cache.match(event.request).then(response => {
-              return response;
-            });
-          });
-      })
-    );
-
-    return;
-  }
-
-  event.respondWith(
-    caches.open(STATIC_CACHE).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request);
-      });
-    })
-  );
+self.addEventListener("fetch", function (e) {
+	console.log("fetch request : " + e.request.url);
+	e.respondWith(
+		caches.match(e.request).then(function (request) {
+			if (request) {
+				console.log("responding with cache : " + e.request.url);
+				return request;
+			} else {
+				console.log("file is not cached, fetching : " + e.request.url);
+				return fetch(e.request);
+			}
+		})
+	);
 });
